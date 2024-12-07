b9f314c051c235cdd087479082036395bf66aff9
Duy <duydang0768134698@gmail.com>
2024-12-07 08:22:55 +0700

    Ex

---
+++ src/Lab_2/Data_Type_Learn.java
@@ -0,0 +1,29 @@
+package Lab_2;
+
+public class Data_Type_Learn {
+    public static void data_type_leaning(String[] args) {
+        // Primitive Data Types
+        byte a = 100; // Stores whole numbers from -128 to 127
+        double b = 820.2304923; // Storing 15 decimal digits
+        int c = 192381023; // Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        boolean d = true;
+        char e = 'a'; // Single character
+        String f = "abc"; //
+        // more like long, short, float,...
+
+        // Java math operators
+        int g = a + a;
+        int h = a - a;
+        int j = a % a;
+        int k = c++; // increase 1
+        int l = c--; // decrease 1
+        // more like >, <, >=, <=, ==, !=
+
+        // Java conditional operators
+        // && and
+        // || or
+        // ! not
+    }
+
+
+}
---
+++ src/Lab_2/If_else_learning.java
@@ -0,0 +1,9 @@
+package Lab_2;
+
+public class If_else_learning {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.print("askldaksd");;
+        }
+    }
+}
