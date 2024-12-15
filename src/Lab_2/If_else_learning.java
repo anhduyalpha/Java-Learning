e202b75f248bb2b97431f9b387142b0b4e3b3cae
Duy <duydang0768134698@gmail.com>
2024-12-15 22:51:41 +0700

    New

--- src/Lab_2/If_else_learning.java
+++ src/Lab_2/If_else_learning.java
@@ -1,9 +1,21 @@
 package Lab_2;
 
+import java.util.Scanner;
+
 public class If_else_learning {
     public static void main(String[] args) {
-        if (true) {
-            System.out.print("askldaksd");;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = scanner.nextInt();
+        if (a == 1) {
+            System.out.print("a is one");;
         }
+        else if (a == 2) {
+            System.out.print("a is 22");;
+        }
+
+        scanner.close();
+
+
     }
 }
